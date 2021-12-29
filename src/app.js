a5787e03b5e89ccaa0ca327cc1fec20db70ccbfd
aclusic <aclusic@gmail.com>
2021-12-29 09:32:26 -0600

    removed console.log s

--- src/app.js
+++ src/app.js
@@ -70,10 +70,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "d3bf41ebab3099c885d2313ad708496f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
